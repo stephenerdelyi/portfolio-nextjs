05c9009855196c0b2293115512542d4c7267ecf6
Steve Erdelyi <stephenerdelyi@icloud.com>
2021-07-30 18:40:30 -0700

    integrating about block

--- blocks/about.js
+++ blocks/about.js
@@ -1,11 +1,17 @@
 import styles from '../styles/blocks/about.module.scss'
 
-export default function About() {
+export default function About(props) {
+    var fields = props.data.fields
+
+    var title = {__html: fields.title}
+    var subtitle = {__html: fields.subtitle}
+    var text_content = {__html: fields.text_content}
+
     return <>
         <div className={styles['block-about']}>
-            <h2 className={styles['block-about__title']}>About</h2>
-        	<h3 className={styles['block-about__subtitle']}>Making beautiful websites is my passion</h3>
-        	<p className={styles['block-about__text']}>No, seriously! I have been creating websites since I built my first web development server in 5<sup>th</sup> grade. Read more about me and my programming adventures below.</p>
+            <h2 className={styles['block-about__title']} dangerouslySetInnerHTML={title}></h2>
+        	<h3 className={styles['block-about__subtitle']} dangerouslySetInnerHTML={subtitle}></h3>
+        	<p className={styles['block-about__text']} dangerouslySetInnerHTML={text_content}></p>
         </div>
     </>
 }
