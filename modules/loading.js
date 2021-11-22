8341e8df991cf6b9b5c2443ac7fcab2428871a4a
Steve Erdelyi <stephenerdelyi@icloud.com>
2021-11-21 22:14:02 -0800

    small bug fix

--- modules/loading.js
+++ modules/loading.js
@@ -1,6 +1,6 @@
 export default function Loading() {
     return <>
-        <div class="loading">
+        <div className="loading">
             <div></div>
             <div></div>
             <div></div>
